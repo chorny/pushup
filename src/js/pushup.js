aa2ed4a57f1bd0be0699630c344a58a320d8f15b
Nick Stakenburg <nickstakenburg@home.nl>
2008-08-26 18:07:14 +0200

    Updated required Firefox to 3 now that Mozilla came out with the Firefox 2 -> 3 upgrade. Updated required Opera to 9.5

--- src/js/pushup.js
+++ src/js/pushup.js
@@ -41,11 +41,11 @@ Pushup.conditions = {
     return version && parseFloat(version[1]) < 7;
   })(navigator.userAgent),
   Firefox: Pushup.Browser.Firefox &&
-    parseFloat(navigator.userAgent.match(/Firefox[\/\s](\d+)/)[1]) < 2,
+    parseFloat(navigator.userAgent.match(/Firefox[\/\s](\d+)/)[1]) < 3,
   Safari: Pushup.Browser.Safari &&
     parseFloat(navigator.userAgent.match(/AppleWebKit\/(\d+)/)[1]) < 500,
   Opera: Pushup.Browser.Opera && (!window.opera.version ||
-    parseFloat(window.opera.version()) < 9.25)
+    parseFloat(window.opera.version()) < 9.5)
 };
 
 (function() {
